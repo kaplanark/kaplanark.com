7f05079e48cd2142912dc93e7aa7b443dbd19ff7
kaplanark <kaplanarkan@gmail.com>
2021-08-31 18:23:14 +0300

    remove post & editing post

--- routes/blog.js
+++ routes/blog.js
@@ -2,6 +2,7 @@ const express = require("express");
 const Blog = require("../models/blog");
 const router = express.Router();
 
+//add new blog
 router.get("/addnewblog",access,(req, res) => {
   res.render("blog/new");
 });
@@ -41,6 +42,54 @@ router.get('/blogs/:blogid', (req,res)=>{
         res.send(err);
     });
 });
+
+//remove blog
+router.get('/delete/:blogid',access,(req,res)=>{  
+  Blog.deleteOne({_id: req.params.blogid}, (err, post)=>{
+      if (err)
+          res.send(err);
+      res.redirect('/admin/all',)
+  });
+});
+
+//edit blog
+router.get('/edit/:blogid',access,(req,res)=>{
+  Blog.findById(req.params.blogid)
+    .then((found)=>{
+        res.render("blog/edit",{found:found})
+    })
+    .catch((err)=>{
+        console.log(err);
+        res.send(err);
+    });
+});
+
+// router.post("/updateblog/:blogid",access,(req, res) => {
+//   console.log(req.body.newdata);
+//   let title = req.body.newdatadata.title;
+//   let subtitle = req.body.newdata.subtitle;
+//   let image = req.body.newdata.image;
+//   let date = req.body.newdata.date;
+//   let blog = req.body.newdata.blog;
+
+//   let updateblog = {
+//     title: title,
+//     subtitle: subtitle,
+//     image: image,
+//     date:date,
+//     blog: blog,
+//   };
+//   Blog.updateOne({_id: req.params.blogid},updateblog)
+//     .then((updateblog) => {
+//       console.log(updateblog);
+//       res.status(201).json(updateblog);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.send(err);
+//     });
+// });
+
 //middleware
 function access(req, res, next) {
     if (req.isAuthenticated()) {
